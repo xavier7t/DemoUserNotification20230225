0adda1d6e46c81f760e25f02e9ab2f81e1378c65
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-25 22:23:22 -0500

    Initial Commit

---
+++ DemoUserNotification20230225/DemoUserNotification20230225App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoUserNotification20230225App.swift
+//  DemoUserNotification20230225
+//
+//  Created by Xavier on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoUserNotification20230225App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
